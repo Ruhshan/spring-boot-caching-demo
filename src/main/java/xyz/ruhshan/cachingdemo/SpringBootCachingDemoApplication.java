e6354d40f35d581c46b4b7040570058c2d96d0a8
Ruhshan <ruhshan.ahmed@gmail.com>
2021-06-02 16:21:57 +0600

    Initialize

---
+++ src/main/java/xyz/ruhshan/cachingdemo/SpringBootCachingDemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.cachingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCachingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ruhshan/cachingdemo/SpringBootCachingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.cachingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCachingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
